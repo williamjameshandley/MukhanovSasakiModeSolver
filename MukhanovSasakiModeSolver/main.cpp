cc6583764f6902f0f061c13a27c5adb0e9956ccc
Ward Haddadin <wh293@cam.ac.uk>
2018-07-03 15:55:01 +0300

    odeint added

---
+++ MukhanovSasakiModeSolver/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include "src/BackgroundSolver.hpp"
+
+int main()
+{
+
+
+}
---
+++ MukhanovSasakiModeSolver/src/BackgroundSolver.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/numeric/odeint.hpp>
+
+
+double H(double phi, double dphi);
+
+double BackgroundSolver()
+{
+    int N = 1000000;
+    double phi_sol[N], dphi_sol[N];
+    
+
+}
