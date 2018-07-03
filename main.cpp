43a1882958c27b7eb7d142c411c5a53160b75735
Will Handley <wh260@cam.ac.uk>
2018-07-03 15:39:19 +0100

    added an example lorenz solver

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <vector>
+
 #include "src/BackgroundSolver.hpp"
+#include <boost/numeric/odeint.hpp>
 
-int main()
+
+const double sigma = 10.0;
+const double R = 28.0;
+const double b = 8.0 / 3.0;
+
+
+void lorenz( const std::vector<double> &x , std::vector<double> &dxdt , double t )
 {
+    dxdt[0] = sigma * ( x[1] - x[0] );
+    dxdt[1] = R * x[0] - x[1] - x[0] * x[2];
+    dxdt[2] = -b * x[2] + x[0] * x[1];
+}
 
+void write_lorenz( const std::vector<double> &x , const double t )
+{
+    std::cout << t << " " << x[0] << " " << x[1] << " " << x[2] << std::endl;
+}
 
+int main(int argc, char **argv)
+{
+    std::vector<double> x = { 10.0 , 1.0 , 1.0 }; // initial conditions
+    boost::numeric::odeint::integrate( lorenz , x , 0.0 , 25.0 , 0.1 , write_lorenz );
 }
