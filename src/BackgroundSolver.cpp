f7fd1bdc22c4d70a91a98fc925cb0b1a9a408ac4
Will Handley <wh260@cam.ac.uk>
2018-07-06 12:07:06 +0100

    Potential fix

--- src/BackgroundSolver.cpp
+++ src/BackgroundSolver.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 #include <math.h>
 #include "ascent/Ascent.h"
 #include "Potential.hpp"
@@ -12,6 +11,9 @@ void BackgroundSolver(double t0, double t1, double phi_p, double dphi_p, Poly po
     std::vector<double> x = {phi_p, dphi_p, eta0, n0};
     
     asc::RK4 integrator;
+    Equations eqs;
+    eqs.pot.m = 1.;
+    eqs.pot.lambda = 0.;
     
     int count = 0;
     
@@ -22,23 +24,23 @@ void BackgroundSolver(double t0, double t1, double phi_p, double dphi_p, Poly po
         {
             std::cout<<t<<"   "<<x[0]<<"   "<<x[1]<<"   "<<x[2]<<"   "<<x[3]<<std::endl;
         }
-        integrator(Equations, x, t, dt);
+        integrator(eqs, x, t, dt);
     }
 
 }
 
 
-double H(double phi, double dphi)
+double Equations::H(double phi, double dphi)
 {
     double H = sqrt((0.5 * dphi * dphi + pot.V(phi)) / 3.0);
     return H;
 }
 
 
-void Equations(const std::vector<double>& x, std::vector<double>& dx_dt, const double)
+void Equations::operator() (const std::vector<double>& x, std::vector<double>& dx_dt, const double)
 {
     dx_dt[0] = x[1];
-    dx_dt[1] = - (3 * H(pot, x[0], x[1]) * x[1] + pot.dV(x[0]));
-    dx_dt[2] = H(pot, x[0], x[1]);
+    dx_dt[1] = - (3 * H(x[0], x[1]) * x[1] + pot.dV(x[0]));
+    dx_dt[2] = H(x[0], x[1]);
     dx_dt[3] = exp(-x[2]);
 }
--- src/BackgroundSolver.hpp
+++ src/BackgroundSolver.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <iostream>
 #include "Potential.hpp"
+#include <vector>
 
 
 double H(double phi, double dphi);
-void Equations(const std::vector<double>& x, std::vector<double>& dx_dt, const double);
 void BackgroundSolver(double t0, double t1, double phi_p, double dphi_p, Poly pot);
+class Equations
+{
+    public:
+        Poly pot;
+        double H(double phi, double dphi);
+        void operator() (const std::vector<double>& x, std::vector<double>& dx_dt, const double);
+};
