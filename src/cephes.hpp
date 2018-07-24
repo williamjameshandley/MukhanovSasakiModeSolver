a8d20495b960a85074338fbffcc9ab54f15442b0
Will Handley <wh260@cam.ac.uk>
2018-07-24 14:21:43 +0100

    Cephes libraries now working

--- src/cephes.hpp
+++ src/cephes.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
 extern "C" int airy ( double, double *, double *, double *, double * );
+extern "C" double jv ( double, double);
+extern "C" double yv ( double, double);
+extern "C" double gamma ( double);
