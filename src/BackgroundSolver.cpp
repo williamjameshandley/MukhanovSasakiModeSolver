e267b5c5dffe3b5df19575e4a9dcd394ace6946f
Will Handley <wh260@cam.ac.uk>
2018-07-31 10:51:59 +0100

    New layout for background solving

--- src/BackgroundSolver.cpp
+++ src/BackgroundSolver.cpp
@@ -1,20 +1,29 @@
 #include <math.h>
 #include "BackgroundSolver.hpp"
 
-double BackgroundSolver::H(double phi, double dphi)
+double H(double phi, double dphi, Potential* pot)
 {
-    return sqrt((0.5 * dphi * dphi + V(phi)) / 3.0);
+    return sqrt((0.5 * dphi * dphi + pot->V(phi)) / 3.0);
 }
 
-double BackgroundSolver::ddz(double phi, double dphi, double n)
+double ddz(double phi, double dphi, double n, Potential * pot)
 {
-    return exp(2 * n) * (2 * H(phi, dphi) * H(phi, dphi)  - (7.0/2) * (dphi * dphi) - ddV(phi) - 2 * dphi * dV(phi) / H(phi, dphi) + 0.5 * (dphi * dphi * dphi * dphi) / (H(phi, dphi) * H(phi, dphi)));
+    return exp(2 * n) * (2 * H(phi, dphi) * H(phi, dphi)  - (7.0/2) * (dphi * dphi) - pot->ddV(phi) - 2 * dphi * pot->dV(phi) / H(phi, dphi) + 0.5 * (dphi * dphi * dphi * dphi) / (H(phi, dphi) * H(phi, dphi)));
 }
 
-void BackgroundSolver::Equations(std::vector<double>& dx_dt, const double, const std::vector<double>& x, double params[])
+void solve_equations(Potential* pot, double t0, double t1, double phi_p, double dphi_p)
 {
+    dlsodar desolver(4, 1);
+    double x[4] = {phi_p, dphi_p, 1.5*t0,0};
+    desolver.integrate(t0, t1, x, equations, pot);
+
+}
+
+void equations(double dx_dt[], const double t, const double x[], void* data)
+{
+    auto pot = static_cast<Potential*>(data);
     dx_dt[0] = x[1];
-    dx_dt[1] = - (3 * H(x[0], x[1]) * x[1] + dV(x[0]));
+    dx_dt[1] = - (3 * H(x[0], x[1]) * x[1] + pot->dV(x[0]));
     dx_dt[2] = H(x[0], x[1]);
     dx_dt[3] = exp(-x[2]);
 }
--- src/BackgroundSolver.hpp
+++ src/BackgroundSolver.hpp
@@ -5,34 +5,11 @@
 #include <boost/numeric/odeint.hpp>
 #include "Potential.hpp"
 
-struct BackgroundSolution
-{
-    BackgroundSolution(std::vector<double>_z,std::vector<double>_dz,std::vector<double>_ddz,std::vector<double>_eta) :
-        z{_z}, dz{_dz}, ddz{_ddz}, eta{_eta} {}
-
-    std::vector<double> z; 
-    std::vector<double> dz; 
-    std::vector<double> ddz; 
-    std::vector<double> eta;
-};
-
-struct BackgroundSolver
-{
-    BackgroundSolver():  pot{nullptr} {}
-
-    template<class Integrator> BackgroundSolution Solve(Integrator, Potential&, double t0, double t1, double phi_p, double dphi_p);
-
-    void Equations(std::vector<double>& dx_dt, const double, const std::vector<double>& x, double params[]);
+void equations(double dx_dt[], const double t, const double x[], void* data);
 
 
-    private:
-        double H(double phi, double dphi);
-        double ddz(double phi, double dphi, double n);
-        double V(double phi)   {return pot->V(phi);}
-        double dV(double phi)  {return pot->dV(phi);}
-        double ddV(double phi) {return pot->ddV(phi);}
-        Potential* pot;
-};
+double H(double phi, double dphi);
+double ddz(double phi, double dphi, double n);
 
 struct Solutions
 {
@@ -49,36 +26,36 @@ struct Solutions
 };
 
 
-template<class Integrator>
-BackgroundSolution BackgroundSolver::Solve(Integrator integrator, Potential& potential, double t0, double t1, double phi_p, double dphi_p)
-{
-    pot = &potential;
-    double dt = (t1 - t0) / 1e6; //used only once at the start of integration
-    double eta0 = 1.5 * t0, n0 = 0;
-    
-    std::vector<double> x = {phi_p, dphi_p, n0, eta0};
-
-    Solutions sol;
-    
-    dlsodar desolver(4, 1);
-    
-    while(x[0] > 0){
-        desolver.integrate(t0, t0 + dt, &x[0], Equations, nullptr);
-        sol(x, t0);
-    }
-    
-    //size_t steps = boost::numeric::odeint::integrate_adaptive(integrator, *this , x0, t0, t1, dt, boost::ref(sol));
-
-    std::vector<double> Z, DZ, DDZ, ETA;
-    
-    for(size_t i = 0; i <= sol.t.size(); i++)
-    {
-        Z.push_back(exp(sol.x[i][2]) * sol.x[i][1] / H(sol.x[i][0], sol.x[i][1]));
-        DZ.push_back(exp(2 * sol.x[i][2]) * (-2 * sol.x[i][1] - dV(sol.x[i][0]) / H(sol.x[i][0], sol.x[i][1]) + std::pow(sol.x[i][1], 3) / (2 * H(sol.x[i][0], sol.x[i][1]) * H(sol.x[i][0], sol.x[i][1]))));
-        DDZ.push_back(ddz(sol.x[i][0], sol.x[i][1], sol.x[i][2]));
-        ETA.push_back(sol.x[i][3]);
-    }
-    pot = nullptr;
-    
-    return BackgroundSolution(Z, DZ, DDZ, ETA);
-}
+//template<class Integrator>
+//BackgroundSolution BackgroundSolver::Solve(Integrator integrator, Potential& potential, double t0, double t1, double phi_p, double dphi_p)
+//{
+//    pot = &potential;
+//    double dt = (t1 - t0) / 1e6; //used only once at the start of integration
+//    double eta0 = 1.5 * t0, n0 = 0;
+//    
+//    std::vector<double> x = {phi_p, dphi_p, n0, eta0};
+//
+//    Solutions sol;
+//    
+//    dlsodar desolver(4, 1);
+//    
+//    while(x[0] > 0){
+//        desolver.integrate(t0, t0 + dt, &x[0], Equations, nullptr);
+//        sol(x, t0);
+//    }
+//    
+//    //size_t steps = boost::numeric::odeint::integrate_adaptive(integrator, *this , x0, t0, t1, dt, boost::ref(sol));
+//
+//    std::vector<double> Z, DZ, DDZ, ETA;
+//    
+//    for(size_t i = 0; i <= sol.t.size(); i++)
+//    {
+//        Z.push_back(exp(sol.x[i][2]) * sol.x[i][1] / H(sol.x[i][0], sol.x[i][1]));
+//        DZ.push_back(exp(2 * sol.x[i][2]) * (-2 * sol.x[i][1] - dV(sol.x[i][0]) / H(sol.x[i][0], sol.x[i][1]) + std::pow(sol.x[i][1], 3) / (2 * H(sol.x[i][0], sol.x[i][1]) * H(sol.x[i][0], sol.x[i][1]))));
+//        DDZ.push_back(ddz(sol.x[i][0], sol.x[i][1], sol.x[i][2]));
+//        ETA.push_back(sol.x[i][3]);
+//    }
+//    pot = nullptr;
+//    
+//    return BackgroundSolution(Z, DZ, DDZ, ETA);
+//}
