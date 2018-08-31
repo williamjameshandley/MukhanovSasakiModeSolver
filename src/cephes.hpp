1594af91768447f209616d9b42771a952df84464
Will Handley <wh260@cam.ac.uk>
2018-08-31 09:26:57 +0100

    Added Bessel I and K functions to cephes

--- src/cephes.hpp
+++ src/cephes.hpp
@@ -3,3 +3,5 @@
 extern "C" int airy ( double, double *, double *, double *, double * );
 extern "C" double jv ( double, double);
 extern "C" double yv ( double, double);
+extern "C" double iv ( double, double);
+extern "C" double kn ( int, double);
