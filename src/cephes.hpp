893c9d97414f98b0e53414cf6a5609fa7037a171
Will Handley <wh260@cam.ac.uk>
2018-09-09 18:32:56 +0100

    Added cephes

--- src/cephes.hpp
+++ src/cephes.hpp
@@ -5,3 +5,12 @@ extern "C" double jv ( double, double);
 extern "C" double yv ( double, double);
 extern "C" double iv ( double, double);
 extern "C" double kn ( int, double);
+
+extern "C" double j0 (double);
+extern "C" double y0 (double);
+extern "C" double i0 (double);
+extern "C" double k0 (double);
+extern "C" double j1 (double);
+extern "C" double y1 (double);
+extern "C" double i1 (double);
+extern "C" double k1 (double);
